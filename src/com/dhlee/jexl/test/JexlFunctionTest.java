fdc3f00ee4c4f3b7073dc9202c7d96eb505ec8ff
dhlee <dhlee@eactive.co.kr>
2022-12-23 16:38:04 +0900

    update lib - hangul enabled

--- src/com/dhlee/jexl/test/JexlFunctionTest.java
+++ src/com/dhlee/jexl/test/JexlFunctionTest.java
@@ -1,5 +1,6 @@
 package com.dhlee.jexl.test;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -105,7 +106,47 @@ public class JexlFunctionTest {
 	    return vars;
 	}
 	
-	private void testSimpleNamespaceFunction(String s1, String s2) {
+	private void testSimpleFunction(String exp, Map<String, Object> varMap) {
+		System.out.println("\n>> testSimpleFunction exp : " + exp);
+		
+		// pre-defined functions
+		Map<String, Object> funcs = new HashMap<String, Object>();
+        
+		// Top level function (null namespace function)
+		funcs.put(null, new SimpleFunction());
+		
+		// use function name -> uf:concat(...)
+		funcs.put("uf", new SimpleFunction());
+		
+		Charset expCharset = Charset.forName("utf-8");
+		
+		System.out.println("Charset = " + expCharset);
+		
+		JexlEngine engine = new JexlBuilder()
+				.charset(expCharset).silent(false).strict(true).safe(false).cache(10000).namespaces(funcs).create();
+		
+		JexlContext jc = new MapContext();
+		
+		JexlScript expr = engine.createScript(exp);
+		List<String> vars = collectVars(expr);
+		System.out.println("# Variables");
+		Object varValue = null;
+		for(String varName:vars) {
+			varValue = varMap.get(varName);
+			if(varValue == null) {
+				System.out.println(String.format("- %s=%s",varName, "<not found in vars>") );
+			}
+			else {
+				jc.set(varName, varValue);
+				System.out.println(String.format("- %s=%s",varName, varValue) );
+			}
+		}
+		
+		Object result = expr.execute(jc);
+	    System.out.println(String.format("%s = %s", exp, result) );	    
+	}
+	
+	private void testSimpleNamespaceFunction(Map<String, Object> varMap) {
 		String exp = "concat(message.group.field1, message.group.field2)";
 		
 		System.out.println("\n>> testSimpleNamespaceFunction exp : " + exp);
@@ -117,7 +158,7 @@ public class JexlFunctionTest {
 		funcs.put(null, new SimpleFunction());
 		
 		// use function name -> uf:concat(...)
-//		funcs.put("uf", new SimpleFunction());
+		funcs.put("uf", new SimpleFunction());
 		
 		JexlEngine engine = new JexlBuilder().silent(false).strict(true).safe(false).cache(10000).namespaces(funcs).create();
 		
@@ -125,62 +166,119 @@ public class JexlFunctionTest {
 		
 		JexlScript expr = engine.createScript(exp);
 		List<String> vars = collectVars(expr);
+		System.out.println("# Variables");
+		Object varValue = null;
 		for(String varName:vars) {
-			System.out.println("# Variables = " + varName);
-			// find var and set to jc
+			varValue = varMap.get(varName);
+			if(varValue == null) {
+				System.out.println(String.format("- %s=%s",varName, "<not found in vars>") );
+			}
+			else {
+				jc.set(varName, varValue);
+				System.out.println(String.format("- %s=%s",varName, varValue) );
+			}
 		}
 		
-		jc.set("message.group.field1", s1);
-		jc.set("message.group.field2", s2);
-
 		Object result = expr.execute(jc);
-	    System.out.println(String.format("%s = %s", exp,result) );	    
+	    System.out.println(String.format("%s = %s", exp, result) );	    
 	}
 	
-	private void testSimpleUserFunction(String s1, String s2) {
+	private void testSimpleUserFunction(Map<String, Object> varMap) {
 		String exp = "uf.concat(message.group.field1, message.group.field2)";
 		
 		System.out.println("\n>> testSimpleUserFunction exp : " + exp);
 		
 		Map<String, Object> funcs = new HashMap<String, Object>();
         
-		JexlEngine engine = new JexlBuilder().silent(false).strict(true).safe(false).cache(10000).namespaces(funcs).create();
+		
+		
+		JexlEngine engine = new JexlBuilder().silent(false)
+				.charset(Charset.defaultCharset()).strict(true).safe(false).cache(10000).namespaces(funcs).create();
+		
+		JexlScript script = engine.createScript(exp);
 		
 		JexlContext jc = new MapContext();
-		
-		JexlScript expr = engine.createScript(exp);
-		Map<String, Object> vars = collectVars(expr, jc);
-		for(String varName:vars.keySet()) {
-			System.out.println("# Variables = " + varName);
-		}
-		
+		List<String> vars = collectVars(script);
 		jc.set("uf", new SimpleFunction());
-		jc.set("message.group.field1", s1);
-		jc.set("message.group.field2", s2);
+		System.out.println("# Variables");
+		Object varValue = null;
+		for(String varName:vars) {
+			varValue = varMap.get(varName);
+			if(varValue == null) {
+				System.out.println(String.format("- %s=%s",varName, "<not found in vars>") );
+			}
+			else {
+				jc.set(varName, varValue);
+				System.out.println(String.format("- %s=%s",varName, varValue) );
+			}
+		}
+		System.out.println("getSourceText = " + script.getSourceText());
+		System.out.println("getParsedText = " + script.getParsedText());
 		
-		JexlExpression e = engine.createExpression(exp);
-		System.out.println("getSourceText = " + e.getSourceText());
-		System.out.println("getParsedText = " + e.getParsedText());
-		
-	    Object result = e.evaluate(jc);
-	    System.out.println(String.format("%s = %s", exp,result) );	    
+	    Object result = script.execute(jc);
+	    System.out.println(String.format("%s = %s", exp, result) );	    
 	}
 	
-	public static void main(String[] args) {
-		JexlFunctionTest example = new JexlFunctionTest();
- //		example.testSimpleMath(10);
-//		example.testJexlScript();
-//		example.testSimpleNamespaceFunction("add1", "more2");
-//		example.testSimpleUserFunction("add1", "more2");
-
+	private void testCache() {
 		// expression
 //		word1 + ' ' + word2 = Hello999999 JEXL999999 594ms
 //		word1 + ' ' + word2 = Hello999999 JEXL999999 13845ms
 		// script
 //		word1 + ' ' + word2 = Hello999999 JEXL999999 644ms
 //		word1 + ' ' + word2 = Hello999999 JEXL999999 14354ms
-		example.testSimpleConcat(true, 1024, "Hello", "JEXL");
-		example.testSimpleConcat(false, 1024, "Hello", "JEXL");
+		testSimpleConcat(true, 1024, "Hello", "JEXL");
+		testSimpleConcat(false, 1024, "Hello", "JEXL");
+	}
+
+	public static void main(String[] args) {
+		JexlFunctionTest example = new JexlFunctionTest();
+ 		example.testSimpleMath(10);
+		example.testJexlScript();
+		
+		Map<String, Object> varMap = new HashMap<String, Object>();
+		
+		varMap.put("message.group.field1", "add1");
+		varMap.put("message.group.field2", "more2");
+		example.testSimpleNamespaceFunction(varMap);
+		example.testSimpleUserFunction(varMap);
+		
+		String exp = null;
+		
+		exp = "concat(group.param1, group.param2)";
+		varMap.clear();
+		varMap.put("group.param1", "안녕");
+		varMap.put("group.param2", "jexl");
+		
+		try {
+			example.testSimpleFunction(exp, varMap);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		exp = "concat(인자1, 인자2)";
+		
+		varMap.clear();
+		varMap.put("인자1", "안녕");
+		varMap.put("인자2", "jexl");
+		try {
+			example.testSimpleFunction(exp, varMap);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		exp = "concat(그룹.인자1, 그룹.인자2)";
+		
+		varMap.clear();
+		varMap.put("그룹.인자1", "안녕");
+		varMap.put("그룹.인자2", "jexl");
+		try {
+			example.testSimpleFunction(exp, varMap);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
 		
 		
 	}
